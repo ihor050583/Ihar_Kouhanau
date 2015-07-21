685e05e59e28aa78a392017322528ca68ccc54cc
Ihar Kouhanau <Ihar_Kouhanau@epam.com>
2015-07-21 21:46:18 +0300

    files was changed

--- app/components/cat/cat.controller.js
+++ app/components/cat/cat.controller.js
@@ -5,22 +5,13 @@ angular
     .controller("catController", catController);
 
 function catController($scope, catService, $filter, $state) {
-        
+
     $scope.click = function (catId) {
-        catService.click(catId);           
+        catService.click(catId);
         $scope.currentCat = catService.getCurrentCat();
     }
 
     $scope.sortOrder = false;
     $scope.cats = catService.getCats();
-    
 
-    $scope.$watch('currentCat', function (newValue, oldValue) {
-        catService.changeCat(newValue);
-    }, true);
-
-    $scope.search = function (searchString) {
-        $scope.cats = $filter('catSearch')(catService.getCats(), searchString);
-    };
-    
-}
+};
